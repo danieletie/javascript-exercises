2a64b416aaa9ed17e15e7618ee389a94f56ab586
danieletie <109879077+danieletie@users.noreply.github.com>
2024-04-06 19:39:52 +0100

    Attempt repeatString.js
    - make 5 tests pass
    - update repeatString.spec.js to pass 5 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, number) {
+    let result = "" ;
+    if (number < 0){
+        result = "ERROR";
+    }
+    for (i = 1; i <= number; i++) {
+        if (i <= number) {
+            result += string;
+        }
+    }
+    return (result)
+}
+repeatString('hey', -1)
 
 // Do not edit below this line
 module.exports = repeatString;

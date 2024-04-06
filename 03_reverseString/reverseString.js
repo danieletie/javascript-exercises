2d1bb403ce5846d65c91416e220fcad8352038a1
danieletie <109879077+danieletie@users.noreply.github.com>
2024-04-06 21:43:17 +0100

    Create reverseString function
    - modify test files to make function pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = string.split("");
+    let stringRev = stringArr.reverse();
+    let newString = stringRev.join("");
+    return newString;
 };
+
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;

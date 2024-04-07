3d54785ee0e1b498c1167b3484896d2143a56363
danieletie <109879077+danieletie@users.noreply.github.com>
2024-04-07 22:38:22 +0100

    Create a function to remove from array
    - modify removeFromArray.spec.js to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    call:
+    for (let arr in array){
+        for (let arg in args) {
+            if (array[arr] === args[arg]) {
+                continue call;
+            }
+        }
+        newArray.push(array[arr]);
+    }
+    return newArray
 };
 
 // Do not edit below this line

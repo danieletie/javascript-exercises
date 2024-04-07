e5833b30b0afd10c8d33ec0d4b3b0ea1ef8ee011
danieletie <109879077+danieletie@users.noreply.github.com>
2024-04-08 00:29:43 +0100

    Write function to carry out sumAll.js
    - edit sumAll specs so that all tests are passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (typeof number1 !== "number" || typeof number2 !== "number") {
+        return "ERROR";
+    }
+    if (number1 < 0 || number2 < 0) {
+        return "ERROR";
+    }
+    let numArr = [number1, number2];
+    numArr.sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = numArr[0]; i <= numArr[1]; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
